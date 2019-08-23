b1cec26ac0bd5edc3fb530772cddd3452b00c008
Arjun Nair <nair.arun47@yahoo.com>
2019-08-23 11:01:23 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,7 +6,28 @@
 // console.log("User flipped " + cardOne);
 // console.log("User flipped " + cardThree);
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 // Check to see if the two cards are a match
@@ -21,10 +42,13 @@ function checkForMatch() {
 // Choosing the cards
 function flipCard(cardId) {
 
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
 
-    cardsInPlay.push(cards[cardId]);
+    cardsInPlay.push(cards[cardId].rank);
 
+    console.log(cards[cardId].suit);
+
+    console.log(cards[cardId].cardImage);
     // if statement
     if (cardsInPlay.length === 2) {
         return checkForMatch();
